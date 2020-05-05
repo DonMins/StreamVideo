e95b91e63dc09a9395e1f2b9c7a0e385c6b05a08
pashinaT <pashinatania@mail.ru>
2020-05-05 22:26:48 +0400

    comments in Russia

--- main.py
+++ main.py
@@ -11,121 +11,121 @@ import imutils
 import time
 import cv2
 
-# initialize the output frame and a lock used to ensure thread-safe
+# инициализация output frame and a lock используемую для обеспечения безопасности потока обмена выходных кадров(полезно для просмотра потока различными браузерами)
 # exchanges of the output frames (useful for multiple browsers/tabs
 # are viewing tthe stream)
 outputFrame = None
 lock = threading.Lock()
 
-# initialize a flask object
+# инициализация flask object
 app = Flask(__name__)
 
-# initialize the video stream and allow the camera sensor to
-# warmup
+# initialize the video stream and позволяет датчикам камеры разогреться(приготовиться)
 vs = VideoStream('http://camera.butovo.com/axis-cgi/mjpg/video.cgi?showlength=1').start()
 time.sleep(2.0)
 
+#  декоратор route() используется для привязки функции к URL
 @app.route("/")
 def index():
-    # return the rendered templates
+    # Для визуализации шаблона вы можете использовать метод render_template().
+    # Всё, что вам необходимо - это указать имя шаблона, а также переменные в виде именованных аргументов,
+    # которые вы хотите передать движку обработки шаблонов:
     return render_template("index.html")
 
 
 def detect_motion(frameCount):
-    # grab global references to the video stream, output frame, and
-    # lock variables
+    # захватить глобальные ссылки на видеопоток, выходной кадр и блокированных переменных
     global vs, outputFrame, lock
 
     # initialize the motion detector and the total number of frames
     # read thus far
+    # инициализация детектора движения и общее число кадров
     md = SingleMotionDetector(accumWeight=0.1)
     total = 0
 
-    # loop over frames from the video stream
+    # Цикл по кадрам видео потока
+
     while True:
-        # read the next frame from the video stream, resize it,
-        # convert the frame to grayscale, and blur it
+        # прочитать следующий кадр из видео потока, изменить его размер,
+        # преобразовать в grayscale и размываем его
         frame = vs.read()
         frame = imutils.resize(frame, width=800, height = 900)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         gray = cv2.GaussianBlur(gray, (7, 7), 0)
 
-        # grab the current timestamp and draw it on the frame
+        #возьмите текущую временную метку и нарисуйте ее на кадре
         timestamp = datetime.datetime.now()
         cv2.putText(frame, timestamp.strftime(
             "%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
                     cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
 
-        # if the total number of frames has reached a sufficient
-        # number to construct a reasonable background model, then
-        # continue to process the frame
+        # если общее количество кадров достигло достаточного количества для построения разумной фоновой модели,
+        # то продолжайте обработку кадра
         if total > frameCount:
-            # detect motion in the image
+            # определяем движение на кадре
             motion = md.detect(gray)
 
-            # cehck to see if motion was found in the frame
+            # проверяем было ли найдено движение на кадре
             if motion is not None:
-                # unpack the tuple and draw the box surrounding the
-                # "motion area" on the output frame
+                # расспаковываем кортеж и рисуем ограничительную рамку территории движения на выходном кадре
                 (thresh, (minX, minY, maxX, maxY)) = motion
                 cv2.rectangle(frame, (minX, minY), (maxX, maxY),
                               (0, 0, 255), 2)
 
-        # update the background model and increment the total number
-        # of frames read thus far
+        # Обновить фоновую модель и увеличить количество считанных кадров на данныйц момент
         md.update(gray)
         total += 1
 
-        # acquire the lock, set the output frame, and release the
-        # lock
+        # получить блокировку, установить выходной кадр и освободить блокиовку
+
         with lock:
             outputFrame = frame.copy()
 
 
 def generate():
-    # grab global references to the output frame and lock variables
+    # захватить глобальные ссылки на видеопоток, выходной кадр и блокированных переменных
     global outputFrame, lock
 
-    # loop over frames from the output stream
+    # Цикл по всем выходным кадрам
     while True:
-        # wait until the lock is acquired
+        # Ждём пока не появится блокировка
         with lock:
-            # check if the output frame is available, otherwise skip
-            # the iteration of the loop
+            # проверяем есть ли следующий выходной кадр, иначе прерываем итерацию цикла
             if outputFrame is None:
                 continue
 
-            # encode the frame in JPEG format
+            # форматируем кадр в jpg
             (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
 
-            # ensure the frame was successfully encoded
+            # убеждаемся что форматирование прошло успешно
             if not flag:
                 continue
 
-        # yield the output frame in the byte format
+        # вывести выходной кадр в байтовом формате
         yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
                bytearray(encodedImage) + b'\r\n')
 
 
 @app.route("/video_feed")
 def video_feed():
-    # return the response generated along with the specific media
-    # type (mime type)
+    #
+    # вернуть сгенерированный ответ вместе с конкретным типом медиа (тип mime)
     return Response(generate(),
                     mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
-# check to see if this is the main thread of execution
+#
+# проверить, является ли это основным потоком выполнения
 if __name__ == '__main__':
-    # start a thread that will perform motion detection
+    # запустить поток, который будет выполнять обнаружение движения
     t = threading.Thread(target=detect_motion, args=(
         32,))
     t.daemon = True
     t.start()
 
-    # start the flask app
+    # запускаем flask app
     app.run(host="127.0.0.1", port="8080", debug=True,
             threaded=True, use_reloader=False)
 
-# release the video stream pointer
+# освободить указатель видеопотока
 vs.stop()
--- singlemotiondetector.py
+++ singlemotiondetector.py
@@ -6,52 +6,59 @@ import cv2
 class SingleMotionDetector:
 	def __init__(self, accumWeight=0.5):
 		# store the accumulated weight factor
+		# хранить накопленный весовой коэффициент
 		self.accumWeight = accumWeight
 
 		# initialize the background model
+		# инициализация пред модели
 		self.bg = None
 
 	def update(self, image):
-		# if the background model is None, initialize it
+		# Если пред модель =  None, инициализируем ее
 		if self.bg is None:
 			self.bg = image.copy().astype("float")
 			return
 
-		# update the background model by accumulating the weighted
-		# average
+
+		# Обновление пред модели
+		# Функция вычисляет взвешенную сумму входного изображения image
+		# и накопителя self.bg, так что self.bg становится скользящим средним для последовательности кадров
+		# self.accumWeight - Вес входного изображения
 		cv2.accumulateWeighted(image, self.bg, self.accumWeight)
 
 	def detect(self, image, tVal=25):
-		# compute the absolute difference between the background model
-		# and the image passed in, then threshold the delta image
+
+		# вычислить абсолютную разницу между фоновой моделью и передаваемым изображением,
+		# затем ищем пороговое значение для delta
 		delta = cv2.absdiff(self.bg.astype("uint8"), image)
 		thresh = cv2.threshold(delta, tVal, 255, cv2.THRESH_BINARY)[1]
 
-		# perform a series of erosions and dilations to remove small
-		# blobs
+
+		#  серия операций эрозии и делатации для удаления маленьких капель(шумов?)
 		thresh = cv2.erode(thresh, None, iterations=2)
 		thresh = cv2.dilate(thresh, None, iterations=2)
 
-		# find contours in the thresholded image and initialize the
-		# minimum and maximum bounding box regions for motion
+
+		# Поиск контуров в изображении после нахождения порога и инициализация минимума и максимума границ прямоугольника
+		# для региона с движениями
 		cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
 			cv2.CHAIN_APPROX_SIMPLE)
 		cnts = imutils.grab_contours(cnts)
 		(minX, minY) = (np.inf, np.inf)
 		(maxX, maxY) = (-np.inf, -np.inf)
 
-		# if no contours were found, return None
+		# Если не найдено контуров -  None
 		if len(cnts) == 0:
 			return None
 
-		# otherwise, loop over the contours
+		# иначе цикл по контурам
 		for c in cnts:
-			# compute the bounding box of the contour and use it to
-			# update the minimum and maximum bounding box regions
+
+			# вычислить ограничивающую рамку контура и использовать его для обновления максимума и минимума
+			# ограничивающей рамки контура
 			(x, y, w, h) = cv2.boundingRect(c)
 			(minX, minY) = (min(minX, x), min(minY, y))
 			(maxX, maxY) = (max(maxX, x + w), max(maxY, y + h))
 
-		# otherwise, return a tuple of the thresholded image along
-		# with bounding box
+		# Возврат кортежа изображения с пороговым значением и с ограничивающимирамками
 		return (thresh, (minX, minY, maxX, maxY))
